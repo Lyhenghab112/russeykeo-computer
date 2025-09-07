14ca50ba9afa7f1506cdd7818a1936edd0f02129
Lyhenghab112 <lyhenghab3@gmail.com>
2025-09-07 17:41:26 +0700

    Prepare for Render deployment - update config and render.yaml

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Config:
         MYSQL_HOST = os.getenv('MYSQL_HOST') or os.getenv('MYSQLHOST') or 'localhost'
         MYSQL_USER = os.getenv('MYSQL_USER') or os.getenv('MYSQLUSER') or 'root'
         MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD') or os.getenv('MYSQLPASSWORD') or '12345'
-        MYSQL_DB = os.getenv('MYSQL_DB') or os.getenv('MYSQL_DATABASE') or os.getenv('MYSQLDATABASE') or 'computershop5'
+        MYSQL_DB = os.getenv('MYSQL_DATABASE') or os.getenv('MYSQL_DB') or os.getenv('MYSQLDATABASE') or 'computershop5'
         MYSQL_PORT = int(os.getenv('MYSQL_PORT') or os.getenv('MYSQLPORT') or 3306)
         SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
